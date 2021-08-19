2f33f8da161469cfd5074f3fe23791f9be6c8450
David Salonga <fdc.salonga@gmail.com>
2021-08-19 15:16:11 +0800

    [Integration-Front&BackEnd][Francis]  test: create test for should return all todos

---
+++ src/test/java/com/example/todolist/integration/TodoIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.example.todolist.integration;
+
+import com.example.todolist.model.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @AfterEach
+    void tearDown() {
+        todoRepository.deleteAll();
+    }
+
+
+    @Test
+    void should_return_all_todos_when_call_get_todos_api() throws Exception {
+        //given
+        final Todo todo = new Todo(1, "First Todo", false);
+        todoRepository.save(todo);
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].text").value("First Todo"))
+                .andExpect(jsonPath("$[0].done").value(false));
+    }
+
+    
+
+
+
+}
