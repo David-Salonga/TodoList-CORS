b11f4aef99caa665303b1d6dadee6c784189ff80
David Salonga <fdc.salonga@gmail.com>
2021-08-19 14:26:30 +0800

    [Integration-Front&BackEnd][Francis]  feat: implement controller for todo

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,2 +1,40 @@
-package com.example.todolist.controller;public class TodoController {
+package com.example.todolist.controller;
+
+import com.example.todolist.model.Todo;
+import com.example.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodo(){
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo);
+    }
+
+//
+//    @PutMapping({"/{id}"})
+//    public Todo updateTodo(@PathVariable String id, @RequestBody Todo todo ){
+//        return todoService.updateTodo(todo);
+//    }
+
+
+
+
 }
