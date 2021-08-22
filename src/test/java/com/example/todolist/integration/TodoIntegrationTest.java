17068242c882e7b1a2859dd20f46abed25b994f7
David Salonga <fdc.salonga@gmail.com>
2021-08-22 18:43:08 +0800

    [Back-end-Todo][Francis] test:added test case for should_create_todo_when_call_add_todo

--- src/test/java/com/example/todolist/integration/TodoIntegrationTest.java
+++ src/test/java/com/example/todolist/integration/TodoIntegrationTest.java
@@ -3,10 +3,12 @@ package com.example.todolist.integration;
 import com.example.todolist.model.Todo;
 import com.example.todolist.repository.TodoRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -22,7 +24,7 @@ public class TodoIntegrationTest {
     @Autowired
     TodoRepository todoRepository;
 
-    @AfterEach
+    @BeforeEach
     void tearDown() {
         todoRepository.deleteAll();
     }
@@ -31,15 +33,14 @@ public class TodoIntegrationTest {
     @Test
     void should_return_all_todos_when_call_get_todos_api() throws Exception {
         //given
-        final Todo todo = new Todo(1, "First Todo", false);
+        final Todo todo = new Todo("first to do item", false);
         todoRepository.save(todo);
 
         //when
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/todos"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(1))
-                .andExpect(jsonPath("$[0].text").value("First Todo"))
+                .andExpect(jsonPath("$[0].text").value("first to do item"))
                 .andExpect(jsonPath("$[0].done").value(false));
     }
 
@@ -56,8 +57,27 @@ public class TodoIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.text").value("First Todo"))
                 .andExpect(jsonPath("$.done").value(false));
-        
     }
+
+    @Test
+    void should_create_todo_when_call_add_todo() throws Exception {
+        //given
+        String todo = "{\n" +
+                "    \"text\": \"new to do item\",\n" +
+                "    \"done\": false\n" +
+                "}";
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/todos")
+                .contentType(MediaType.APPLICATION_JSON).content(todo))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.text").value("new to do item"))
+                .andExpect(jsonPath("$.done").value(false));
+
+    }
+
+
+
     
     
 }
