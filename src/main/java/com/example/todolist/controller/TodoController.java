fdc820b8306fa68a60961fd76a3f0bccbd46393a
David Salonga <fdc.salonga@gmail.com>
2021-08-22 18:43:52 +0800

    [Back-end-Todo][Francis] feat: added http response for post mapping

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todolist.controller;
 import com.example.todolist.model.Todo;
 import com.example.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,7 +29,9 @@ public class TodoController {
         return todoService.getById(id);
     }
 
+
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Todo addTodo(@RequestBody Todo todo){
         return todoService.addTodo(todo);
     }
